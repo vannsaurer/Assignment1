cedbffa728bbc153f8c691f1870864c229dc8306
Virginia <virginiaanns@gmail.com>
2018-09-17 20:17:34 -0400

    Added CardTest

---
+++ src/test/java/CardTest.java
@@ -0,0 +1,15 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class CardTest extends TestCase {
+  @Test
+  public void getSuitAsString() {
+    Card card = new Card(10,1);
+    assertEquals(10, card.getSuitAsString());
+  }
+
+
+
+
+}
