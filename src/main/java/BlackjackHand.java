ae6ee24f0446eddb0e6259eaca637e51c73a934b
Virginia <virginiaanns@gmail.com>
2018-09-19 00:54:53 -0400

    Added the method to BlackjackHand.java

---
+++ src/main/java/BlackjackHand.java
@@ -0,0 +1,53 @@
+
+/*
+  This subclass of the Hand class represents a hand of cards
+   in the game
+  It's method getBlackjackHand(), this returns the value of the hand
+*/
+
+
+public class BlackjackHand extends Hand {
+
+
+  public int getBlackjackValue() {
+    // returns the value of this hand
+
+    int val;
+    boolean ace;  // This will be set to true if the
+
+    // hand contains an ace.
+
+    int cards;    // Number of cards in the hand.
+    val = 0;
+    ace = false;
+    cards = getCardCount();
+
+    for ( int i = 0;  i < cards;  i++ ) {
+      // Add the value of the i-th card in the hand.
+
+      Card card;    // The i-th card;
+      int cardVal;  // The blackjack value of the i-th card.
+      card = getCard(i);
+      cardVal = card.getValue();
+      if (cardVal > 10) {
+        cardVal = 10;   // For a Jack, Queen, or King.
+      }
+      if (cardVal == 1) {
+        ace = true;     // There is at least one ace.
+      }
+      val = val + cardVal;
+    }
+    // Now, val is the value of the hand, counting any ace as 1.
+    // If there is an ace, and if changing its value from 1 to
+    // 11 would leave the score less than or equal to 21,
+    // then do so by adding the extra 10 points to val.
+    if ( ace == true  &&  val + 10 <= 21 )
+      val = val + 10;
+    return val;
+
+  }
+
+
+}
+
+
