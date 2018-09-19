734dc568bbb40085cc16ba396abdc5d1582fef73
Virginia <virginiaanns@gmail.com>
2018-09-18 23:20:54 -0400

    Added more tests for testing card values in CardTest

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -7,13 +7,22 @@ public class CardTest extends TestCase {
   public void testGetSuitAsString() {
     Card card = new Card(10,1);
     assertEquals("H", card.getSuitAsString());
-
   }
   @Test
   public void testGetValueAsString(){
-    Card card = new Card(2,3);
-    assertEquals("2", card.getValueAsString());
+    Card card = new Card(1,3);
+    assertEquals("A", card.getValueAsString());
+
+    Card card1 = new Card( 11,2);
+    assertEquals("J", card1.getValueAsString());
+
+    Card card2 = new Card( 12,2);
+    assertEquals("Q", card2.getValueAsString());
+
+    Card card3 = new Card( 13,2);
+    assertEquals("K", card3.getValueAsString());
   }
+
   @Test
   public void testToString() {
     Card card = new Card(10,1);
