4b6cd6b0a9234ec30b42f0f948514d79c3768e4e
Virginia <virginiaanns@gmail.com>
2018-09-18 23:35:08 -0400

    Fixed up tests in HandTest.java and updated Hand.java

--- src/test/java/HandTest.java
+++ src/test/java/HandTest.java
@@ -17,44 +17,7 @@ public class HandTest extends TestCase{
     hand.removeCard(0);
     assertEquals( 0, hand.getCardCount());
   }
-  /*@Test
-  public void testGetCard() {
-    Hand hand = new Hand();
-    Card card = new Card(9,2);
-    hand.addCard(card);
-
-    assertEquals( "D9", hand.getCard(0));
-  }*/
-
-  @Test
-  public void testSortBySuit(){
-    Hand hand = new Hand();
-    Card card1 = new Card(9,2);
-    Card card2 = new Card(8,1);
-    Card card3 = new Card(2,1);
-    hand.addCard(card1);
-    hand.addCard(card2);
-    hand.addCard(card2);
-
-    assertEquals("H2,H8,D9",hand.toString());
-  }
-
-
-  @Test
-  public void testSortByValue(){
-    Hand hand = new Hand();
-    Card card1 = new Card(2,2);
-    Card card2 = new Card(8,1);
-    Card card3 = new Card(2,1);
-    hand.addCard(card1);
-    hand.addCard(card2);
-    hand.addCard(card2);
-
-    assertEquals("H2D9H8", hand);
-  }
-
-
-
+  
 
 
 }
